46fabe9e4c1311936125f61d0f6b1c88e77010df
Zoe Febrero <zoe@butterflowy.com>
2025-06-01 03:27:07 +0800

    Remove non-existent Partners page references

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ import Dashboard from './components/Dashboard';
 import Documentation from './pages/Documentation';
 import Pricing from './pages/Pricing';
 import Blog from './pages/Blog';
+import Features from './pages/Features';
+import Integrations from './pages/Integrations';
+import Enterprise from './pages/Enterprise';
+import About from './pages/About';
+import Careers from './pages/Careers';
+import Contact from './pages/Contact';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+import Security from './pages/Security';
+import Compliance from './pages/Compliance';
+import Guides from './pages/Guides';
+import Changelog from './pages/Changelog';
 import './index.css';
 
 function App() {
@@ -18,6 +30,18 @@ function App() {
           <Route path="/docs" element={<Documentation />} />
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/blog" element={<Blog />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/integrations" element={<Integrations />} />
+          <Route path="/enterprise" element={<Enterprise />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/security" element={<Security />} />
+          <Route path="/compliance" element={<Compliance />} />
+          <Route path="/guides" element={<Guides />} />
+          <Route path="/changelog" element={<Changelog />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>

70d0b172140ceaed7f05603553120bd9d3472ff1
Zoe Febrero <zoe@butterflowy.com>
2025-06-09 10:01:03 +0800

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
           <div className="flex items-center space-x-3 md:col-span-3 mb-8">
             <div className="h-8 w-8 flex items-center justify-center">
               <img 
-                src={theme === 'light' ? '/logo dark mode.png' : '/logo light mode.png'} 
+                src={theme === 'light' ? '/logo light mode.png' : '/logo dark mode.png'} 
                 alt="Velkros Logo" 
                 className="h-6 w-6 object-contain"
               />
